0da2d445160f511aa02ca9951d6244707008bbb4
Yao Jian Yap <yapyaojian731@gmail.com>
2019-09-29 13:19:48 +0800

    Added SpinKit for loading animation in Loading Screen

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,10 +1,9 @@
-import 'dart:convert';
-
 import 'package:clima/keys.dart';
+import 'package:clima/screens/location_screen.dart';
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -31,6 +30,10 @@ class _LoadingScreenState extends State<LoadingScreen> {
     double temperature = weatherData['main']['temp'];
     int condition = weatherData['weather'][0]['id'];
     String cityName = weatherData['name'];
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    }));
   }
 
   @override
@@ -43,7 +46,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(),
+      body: Center(
+        child: SpinKitFadingFour(color: Colors.white, size: 100),
+      ),
     );
   }
 }
